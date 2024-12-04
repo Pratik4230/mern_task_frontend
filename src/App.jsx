1bfed45c57800bf2f94da45101998e47d2631c05
Pratik4230 <pratikjadhav1438@gmail.com>
2024-12-04 09:31:43 +0530

    get started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <section>
+          <h1>Hello</h1>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
