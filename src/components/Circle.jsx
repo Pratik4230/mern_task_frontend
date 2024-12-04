b7c194b53de1d667caaaba6271ae67762b2fd1c0
Pratik4230 <pratikjadhav1438@gmail.com>
2024-12-04 16:56:22 +0530

    circle - random dashbord  component

---
+++ src/components/Circle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Circle = () => {
+  return (
+    <>
+      <div className="bg-blue-500 rounded-full h-44 w-44 flex justify-center items-center">
+        <p className="text-white text-2xl font-medium flex justify-center items-center p-1">
+          Transaction Dashboard
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Circle;
